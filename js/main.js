a7bbfa36cd2df00fcb9aa887b1c9d51aa283d3bc
Laura Martin gonzalez <lauramartingonzalez@MacBook-Pro-de-Laura.local>
2022-06-27 11:19:57 +0200

    exercise-3

--- js/main.js
+++ js/main.js
@@ -21,39 +21,67 @@ form.classList.add("js-new-form")
 //     </p>
 // </article>
 // </li>`;
-
+const list = document.querySelector(".list");
 const kittenImage = "https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg";
 const kittenName = "Anastacio";
 const kittenRace = "British Shorthair";
 const kittenDesc = " Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle";
 
-const kittenOne = document.querySelector(".card");
-kittenOne.innerHTML = `<li class="card">
+
+const kittenImage2 = "https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg";
+const kittenName2 = "Anastacio";
+const kittenRace2 = "British Shorthair";
+const kittenDesc2 = " Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle";
+
+
+
+
+const kittenImage3 = "https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg";
+const kittenName3 = "Fiona";
+const kittenDesc3 =
+    "Ruiseño, juguetón, cariñoso le gusta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
+const kittenRace3 = "British Shorthair";
+
+
+const input_search_desc = document.querySelector('.js_in_search_desc');
+input_search_desc.value = 'cariñoso';
+const descrSearchText = input_search_desc.value;
+if (kittenDesc.includes(descrSearchText)) {
+    list.innerHTML += `<li class="card">
 <article>
     <img class="card_img" src=${kittenImage} alt="gatito" />
     <h3 class="card_title">${kittenName}</h3>
-    <h4 class="card_race">${kittenRace}</h4>
+    <h4 class="card_race">${kittenDesc}</h4>
     <p class="card_description">
-        ${kittenDesc}
+        ${kittenRace}
     </p>
 </article>
 </li>`;
+}
 
+if (kittenDesc2.includes(descrSearchText)) {
+    list.innerHTML += `<li class="card">
+    <article>
+        <img class="card_img" src=${kittenImage2} alt="gatito" />
+        <h3 class="card_title">${kittenName2}</h3>
+        <h4 class="card_race">${kittenDesc2}</h4>
+        <p class="card_description">
+            ${kittenRace2}
+        </p>
+    </article>
+    </li>`;
+}
 
-const kittenImage1 = "https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg";
-const kittenName1 = "Fiona";
-const kittenDesc1 =
-  "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
-const kittenRace1 = "British Shorthair";
+if (kittenDesc3.includes(descrSearchText)) {
+    list.innerHTML += `<li class="card">
+    <article>
+        <img class="card_img" src=${kittenImage3} alt="gatito" />
+        <h3 class="card_title">${kittenName3}</h3>
+        <h4 class="card_race">${kittenDesc3}</h4>
+        <p class="card_description">
+            ${kittenRace3}
+        </p>
+    </article>
+    </li>`;
 
-const kittenTwo = document.querySelector(".card");
-kittenOne.innerHTML = `<li class="card">
-<article>
-    <img class="card_img" src=${kittenImage1} alt="gatito" />
-    <h3 class="card_title">${kittenName1}</h3>
-    <h4 class="card_race">${kittenDesc1}</h4>
-    <p class="card_description">
-        ${kittenRace1}
-    </p>
-</article>
-</li>`;
+}
